8fde4ff145f5e2e6713593a0c5c1dee10be9e25b
Raul Resendez <raul.resendez@proton.me>
2024-10-23 14:41:39 -0500

    Complete project 7.6

---
+++ Ch7_Basic_Types/7.6/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+
+    printf("The size of an Int is %lu bytes\n", sizeof(int));
+    printf("The size of a Short is %lu bytes\n", sizeof(short));
+    printf("The size of a long is %lu bytes\n", sizeof(long));
+    printf("The size of a float is %lu bytes\n", sizeof(float));
+    printf("The size of a double is %lu bytes\n", sizeof(double));
+    printf("The size of a long double is %lu bytes\n", sizeof(double));
+
+    return 0;
+}
