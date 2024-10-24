eb8f871eb45d023bc7a8e01de6d8cbac810d2898
Raul Resendez <raul.resendez@proton.me>
2024-10-24 00:31:04 -0500

    Incomplete implementation of a digital clock - exercise is correct though

---
+++ Ch8_Arrays/Exercises/Exc_9/electronic_clock.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+    const int segments[10][7]={{1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 0, 0, 0, 0}, {1, 1, 0, 1, 1, 0, 1}, {1, 1, 1, 1, 0, 0, 1},
+    {0, 1, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 0, 1, 1}, {1, 0, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 1, 1}};
+
+    const char representation[7]={'_', '|', '|', '_', '|', '|', '_'};
+    const char indentation[]={' ','_', '\n', '|', '_', '|', '\n', '|', '_', '|'};
+
+    const char digit_seg_1_on[]={' ','_', '\n'};
+    const char digit_seg_1_off[]={' ',' ', '\n'};
+
+    const char digit_seg_2_on[] = {'|'};
+    const char digit_seg_2_off[] = {' '};
+
+    for (int i = 0; i<10; i++){
+        printf("%c", indentation[i]);
+    }
+    return 0;
+}
