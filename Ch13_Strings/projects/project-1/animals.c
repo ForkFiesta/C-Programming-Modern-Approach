2a74b67ca8c8cfbc6313f4b52efa163f6ab8cb17
Raul Resendez <raul.resendez@proton.me>
2024-10-28 07:41:22 -0500

    simplified with string.h functionality

--- Ch13_Strings/projects/project-1/animals.c
+++ Ch13_Strings/projects/project-1/animals.c
@@ -5,74 +5,29 @@
 #define MAX_SIZE 20
 
 
-int read_line(char str[], int n){
-    int ch, i = 0;
-
-    while((ch=getchar())!='\n'){
-        if(i<n) str[i++] = ch;
-    }
-    str[i] = '\0';
-    return i;
-}
 int main(){
-    int longest_word = 0;
-    int shortest_word = 20;
+
+    int word_length;
     char largest_word[MAX_SIZE];
     char smallest_word[MAX_SIZE];
     char temp_word[MAX_SIZE];
-    char *l = largest_word;
-    char *t = temp_word;
-    char *s = smallest_word;
-    
 
-    int word_length;
     while(word_length != 4){
-
-        printf("Enter word: ");
-        word_length = read_line(temp_word, MAX_SIZE);
-        char *l = largest_word;
-        char *t = temp_word;
-        char *s = smallest_word;
-        if (word_length>longest_word){
-            longest_word = word_length;
-            while (*l != '\0'){
-                l++;
-                while(*t != '\0'){
-                    *l = *t;
-                    l++;
-                    t++;
-                }
-
-            }
-            *l = '\0';
+        printf("Enter a word: ");
+        scanf("%s", temp_word);
+        word_length = strlen(temp_word);
+        if(word_length>strlen(largest_word)){
+            strcpy(&largest_word[0], temp_word);
         }
-        t = temp_word;
-        if (word_length < shortest_word){
-            shortest_word = word_length;
-            while(*s != '\0'){
-                s++;
-                while(*t != '\0'){
-                    *s = *t;
-                    s++;
-                    t++;
-                }
-            }
-            *s = '\0';
-
-
+        if (word_length<strlen(smallest_word)){
+            strncpy(&smallest_word[0], temp_word, word_length);
+            // smallest_word[word_length+1] = '\0';
         }
+
     }
 
+    printf("Largest word: %s\n", largest_word);
     printf("Smallest word: %s\n", smallest_word);
-    printf("Largest Word: %s\n", largest_word);
-    
-        
-    
-
-
-
-    
-
 
     return 0;
 }
