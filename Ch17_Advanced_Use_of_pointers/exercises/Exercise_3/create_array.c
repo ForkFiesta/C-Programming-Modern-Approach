927e688bcaf897f7346ef01028caef3dfd61517e
Raul Resendez <raul.resendez@proton.me>
2024-10-29 08:17:26 -0500

    Complete exercise 3

---
+++ Ch17_Advanced_Use_of_pointers/exercises/Exercise_3/create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int *create_array(int n, int initial_value);
+
+int main(){
+
+
+    int *arr = create_array(100, 7);
+    if(arr == NULL){
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 1; i <= 100; i++){
+        printf("%d. %d\n", i, arr[i]);
+    }
+
+
+    return 0;
+}
+
+int *create_array(int n, int initial_value){
+    int *new_arr = malloc(sizeof(int)*(n+1));
+    if(new_arr == NULL){
+        return new_arr;
+    }
+    for(int i = 0; i<n+1;i++){
+        new_arr[i] = initial_value;
+    }
+    return new_arr;
+}
